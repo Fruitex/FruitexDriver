52d16d706fcf553ef36bdfed4d5c90dd941bf19c
Greg Wang <gregwym@gmail.com>
2013-11-05 19:45:13 -0500

    Use Tab-base navigation. Add DeliverySummaryVC and hook up.

---
+++ FruitexDriver/ViewControllers/FDDeliverySummaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  FDDeliverySummaryViewController.h
+//  FruitexDriver
+//
+//  Created by Greg on 11/5/2013.
+//  Copyright (c) 2013 Fruitex. All rights reserved.
+//
+
+@interface FDDeliverySummaryViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
+
+@end
---
+++ FruitexDriver/ViewControllers/FDDeliverySummaryViewController.m
@@ -0,0 +1,71 @@
+//
+//  FDDeliverySummaryViewController.m
+//  FruitexDriver
+//
+//  Created by Greg on 11/5/2013.
+//  Copyright (c) 2013 Fruitex. All rights reserved.
+//
+
+#import "FDDeliverySummaryViewController.h"
+
+typedef enum {
+    SummaryViewStoreSection = 0,
+    SummaryViewOrderSection,
+    SummaryViewNumOfSections
+} SummaryViewSections;
+
+@interface FDDeliverySummaryViewController ()
+
+@end
+
+@implementation FDDeliverySummaryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    self.title = @"Delivery Summary";
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return SummaryViewNumOfSections;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+}
+
+@end
