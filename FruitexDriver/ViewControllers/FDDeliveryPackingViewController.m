fcc2f2289122d9c145616e95ef5759ea96648240
Greg Wang <gregwym@gmail.com>
2013-11-08 13:53:39 -0500

    Title every ViewController.

--- FruitexDriver/ViewControllers/FDDeliveryPackingViewController.m
+++ FruitexDriver/ViewControllers/FDDeliveryPackingViewController.m
@@ -33,6 +33,9 @@
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
+    self.title = @"Packing";
+    [self packedOrderItemsDidChange];
 }
 
 - (void)didReceiveMemoryWarning
